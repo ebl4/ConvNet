57b673e5d63134e3924f079c0799c003016ada6b
Edson <edbdelima@gmail.com>
2016-10-14 00:54:14 -0300

    Convolution function modified

--- ConvNet/conv.c
+++ ConvNet/conv.c
@@ -31,7 +31,7 @@ void initSlice(int* v, int start, int end){
 }
 
 int* initFilter(int** filter, int depth, int filterSize){
-	int *f, *result;
+	int *f;
 	f = (int *) malloc(sizeof(int)*filterSize*filterSize);
 	f = filter[depth];
 //	free(f);
@@ -40,8 +40,11 @@ int* initFilter(int** filter, int depth, int filterSize){
 }
 
 void convolution(int** input, int** filter, int inputSize, int filterSize, int stride)){
+	int result = 0;
 	int slide = spatialSize(inputSize, filterSize, 0, stride);
 	int *v, *f, *matrix;
+	int depth = 0;
+
 	matrix = (int *) malloc(sizeof(int)*inputSize*inputSize);
 	v = (int *) malloc(sizeof(int)*filterSize*filterSize);
 	f = (int *) malloc(sizeof(int)*filterSize*filterSize);
@@ -50,21 +53,25 @@ void convolution(int** input, int** filter, int inputSize, int filterSize, int s
 	matrix = input[depth];
 	f = initFilter(filter, depth, filterSize);
 
-	//inicializando v, fatia da matriz de entrada
-	int depth = 0;
-	for (int i = 0; i < inputSize*inputSize; ++i)
-	{
-		int start = 0, end = 0;
-		for (int j = start; j < filterSize; ++j)
+	//inicializando v, fatia da matriz de entrada	
+	for (int r = 0; r < slide; ++r){
+		offset = r*inputSize;
+		for (int i = 0; i < slide; ++i)
 		{
-			l1 = j*inputSize;
-			l2 = j*filterSize;
-			for (int k = 0; k < filterSize; ++k)
+			int start = offset+i, end = start+filterSize-1;
+			for (int j = start; j < end; ++j)
 			{
-				v[l2+k] = matrix[start+l1+k];
+				//offsets
+				l1 = j*inputSize;
+				l2 = j*filterSize;
+				for (int k = 0; k < filterSize; ++k)
+				{
+					v[l2+k] = matrix[start+l1+k];
+				}
 			}
+			result = convolv(v, f, filterSize, filterSize);
+			printf("%d\n", result);
 		}
-		convolv(v, f, filterSize, filterSize);
 	}
 
 }
@@ -74,19 +81,19 @@ int main(int argc, char const *argv[])
 	/* code */
 	int res;
 	int **a, **b;
-	int *m1, *m2, *m3, m = 2, n = 2;
-	m1 = (int *) malloc(sizeof(int)*m*n);
-	m2 = (int *) malloc(sizeof(int)*m*n);
-	m3 = (int *) malloc(sizeof(int)*m*n);
+	int *m1, *m2, *m3, m = 3, n = 2;
+	m1 = (int *) malloc(sizeof(int)*m*m);
+	m2 = (int *) malloc(sizeof(int)*n*n);
+	//m3 = (int *) malloc(sizeof(int)*m*n);
 
 	a = (int **) malloc(sizeof(int*));
 	b = (int **) malloc(sizeof(int*));
 
 
-	m1[0] = 2;
-	m1[1] = 3;
-	m1[2] = 2;
-	m1[3] = 3;
+	m1[0] = 2;m1[1] = 3;m1[2] = 3;	
+	m1[3] = 2;m1[4] = 3;m1[5] = 2;		
+	m1[6] = 3;m1[7] = 2;m1[8] = 2;
+
 
 	m2[0] = 2;
 	m2[1] = 3;
@@ -96,10 +103,13 @@ int main(int argc, char const *argv[])
 	a[0] = m1;
 	b[0] = m2;
 	//res = convolv(m1, m2, 4);
-	res = testConv(a, b, 1, 2);
+	//res = testConv(a, b, 1, 2);
+
+	convolution(a, b, 3, 2, 1);
+
 	free(m1);free(m2);free(m3);free(a);free(b);
 
-	printf("%d\n", res);
+	//printf("%d\n", res);
 
 	return 0;
 }
---
+++ ConvNet/convLayer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Largura da imagem de entrada
+*/
+int W1;
+
+/*
+Altura da imagem de entrada
+*/
+int H1;
+/*
+Profundidade da imagem de entrada
+*/
+int D1;
+
+int main(){
+
+return 0;
+}
