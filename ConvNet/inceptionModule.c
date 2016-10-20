4cb8f4271f914e9fb65b7ccb4f22d18b8e3a9f5d
edson.barboza <edson.barboza@basis.com.br>
2016-10-20 16:06:04 -0200

    Update parameters init in inception module

--- ConvNet/inceptionModule.c
+++ ConvNet/inceptionModule.c
@@ -31,12 +31,32 @@ void convolutionPart(int** input, int*** filter, int* biases, int numFilters,
 
 }
 
+void initStrides(int *strides, int stridesArraySize, int numStrides){
+	for(int i = 0; i < stridesArraySize; ++i){
+		strides[i] = numStrides;
+	}
+}
+
+void initPaddings(int *paddings, int paddingArraySize, int numPaddings){
+	for(int i = 0; i < paddingArraySize; ++i){
+		paddings[i] = numPaddings;
+	}
+}
+
+void initSlices(int *slices, int inputSize, int numSlices, int *filterSize, int *paddings, int *strides){
+	for(int i = 0; i < numSlices; ++i){
+		slices[i] = spatialSize(inputSize, filterSize[i], paddings[i], strides[i]);
+	}
+}
+
 void naiveInception(int **input, int **output, int inputSize, int ****filters, int *numFilters, int numWeigths, int* filterSize, int* depths, int **biases){
 	int numWeigthsPerConv, numWeigthsConvOne = 0, remain = 0, stride = 1, dimOutputConcat = 0, numFiltersConcat = 0;
 	//int *numFilters = (int *) malloc(sizeof(int)*NUM_CONV);
 	int ***result = (int ***) malloc(sizeof(int**)*NUM_CONV);
-	int *slices;
-	int ***outputConcat;
+	int *slices = (int *) malloc(sizeof(int**)*NUM_CONV);
+	int *strides = (int *) malloc(sizeof(int**)*NUM_CONV);
+	int *paddings = (int *) malloc(sizeof(int**)*NUM_CONV);
+	int ***outputConcat = (int ***) malloc(sizeof(int**)*(NUM_CONV-1));
 
 	if(numWeigths%MULT_SIZE_WEIGTHS == 0){
 		numWeigthsPerConv = numWeigths/MULT_SIZE_WEIGTHS;
@@ -47,6 +67,10 @@ void naiveInception(int **input, int **output, int inputSize, int ****filters, i
 		numWeigthsPerConv = (numWeigths - remain)/MULT_SIZE_WEIGTHS;
 	}	
 	numWeigthsConvOne = numWeigthsPerConv+remain;
+	
+	initStrides(strides, NUM_CONV, 1);
+	initPaddings(paddings, NUM_CONV, 0);
+	initSlices(slices, inputSize, NUM_CONV, filterSize, paddings);
 
 	//5x5 convolution
 	result[0] = multiFilterConvolution(input, filters[0], biases[0], numFilters[0], inputSize, filterSize[0], depths[0], stride);
@@ -55,6 +79,7 @@ void naiveInception(int **input, int **output, int inputSize, int ****filters, i
 	//1x1 convolution
 	result[2] = multiFilterConvolution(input, filters[2], biases[2], numFilters[2], inputSize, filterSize[2], depths[2], stride);
 	//3x3 max pooling
+
 	
 	depthConcatLayers(result[0], result[1], outputConcat[0], numFilters[0], numFilters[1], slices[0], slices[1]);
 	dimOutputConcat = max(slices[0], slices[1]);
@@ -62,6 +87,7 @@ void naiveInception(int **input, int **output, int inputSize, int ****filters, i
 	outputConcat[1] = (int **) malloc(sizeof(int*)*(numFiltersConcat + numFilters[2]));
 	depthConcatLayers(outputConcat[0], result[2], outputConcat[1], numFiltersConcat, numFilters[2], dimOutputConcat, slices[2]);
 
+	output = outputConcat[1];
 }
 
 
